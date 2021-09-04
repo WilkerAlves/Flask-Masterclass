f16951f14b66a6e27a3769927c4aaa9284035127
WilkerAlves <wilkeralves97@gmail.com>
2021-09-04 10:38:40 -0300

    finalizando implementaçao do flask-sqlalchemy

---
+++ database-manager/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(84), nullable=False)
+    email = db.Column(db.String(84), nullable=False, unique=True, index=True)
+    profile = db.relationship("Profile", backref='user', uselist=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(db.Model):
+    __tablename__ = "profiles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.Unicode(124), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __str__(self):
+        return self.name
+
+
+@app.route("/")
+def index():
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route("/user/<int:id>")
+def unique(id):
+    user = User.query.get(id)
+    return render_template("user.html", user=user)
+
+
+@app.route("/user/delete/<int:id>")
+def delete(id):
+    user = User.query.filter_by(id=id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
